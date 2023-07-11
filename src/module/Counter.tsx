23c3cc13205741fa4bf963b21f921b026bde96d5
Pandula <pandula95@gmail.com>
2023-07-11 22:37:56 +0530

    style: :lipstick: Implement UI elements and CSS

--- src/module/Counter.tsx
+++ src/module/Counter.tsx
@@ -1,5 +1,8 @@
-"use client"
+"use client";
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./Counter.css";
 
 const Counter: React.FC = () => {
   const [count, setCount] = useState(0);
@@ -8,8 +11,12 @@ const Counter: React.FC = () => {
   const increaseHandler = () => {
     if (count < limit) {
       setCount(count + 1);
-    } else {
-      alert(`Count cannot exceed ${limit}`);
+    }
+
+    if (count === limit) {
+      toast.error(`Count cannot exceed ${limit}`, {
+        position: toast.POSITION.TOP_CENTER,
+      });
     }
   };
 
@@ -17,7 +24,9 @@ const Counter: React.FC = () => {
     if (count > 0) {
       setCount(count - 1);
     } else {
-      alert("Count can't be less than 0");
+      toast.error(`Count cannot decrese bellow 0`, {
+        position: toast.POSITION.TOP_CENTER,
+      });
     }
   };
 
@@ -26,11 +35,34 @@ const Counter: React.FC = () => {
   };
 
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={increaseHandler}>Increase</button>
-      <button onClick={decreaseHandler}>Decrease</button>
-      <button onClick={resetHandler}>Reset</button>
+    <div className="container">
+      <div className="card">
+        <div className="card-content">
+          <ToastContainer />
+          <p className="count-text">Count: {count}</p>
+          <div className="button-container">
+            <button
+              //   className={`action-button ${count >= limit ? "disabled" : ""}`}
+              onClick={increaseHandler}
+              className="action-button"
+              //   disabled={count >= limit}
+            >
+              Increase
+            </button>
+            <button
+              //   className={`action-button ${count === 0 ? "disabled" : ""}`}
+              className="action-button"
+              onClick={decreaseHandler}
+              //   disabled={count === 0}
+            >
+              Decrease
+            </button>
+            <button className="action-button" onClick={resetHandler}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
